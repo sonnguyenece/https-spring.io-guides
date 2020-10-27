fcfa07633540def0072ee0f787576d779995a525
nmson <nmson@ecpay.vn>
2020-10-27 18:47:03 +0700

    run springboot project

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -9,10 +9,19 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
     private static final String template = "Hello,%s!";
+
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+    /**
+     AtomicLong dùng để tạo số tăng dần
+     Tạo file jar bằng mvn clean package
+     Có thể chạy spring boot bằng lệnh:
+     - mvn spring-boot:run
+     - chạy intellij
+     - java -jar filePath/file.jar
+     */
 }
