babd0adc4e4afa932b0b4b1bd3f23779fb35002e
nmson <nmson@ecpay.vn>
2020-10-29 18:30:52 +0700

    @JsonProperty @JsonIgnoreProperties

--- src/main/java/com/example/consumingrest/Quote.java
+++ src/main/java/com/example/consumingrest/Quote.java
@@ -1,11 +1,26 @@
 package com.example.consumingrest;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+/*  @JsonIgnoreProperties để báo hiệu trường nào ko map được thì kệ cmn
+
+ */
+
 @JsonIgnoreProperties(ignoreUnknown = true)
+
 @Data
 public class Quote {
-    private String type;
-    private Value value;
+    //    private String type;
+    //    private Value value;
+
+// @JsonProperty để chỉ định chính xác cái từ khóa của tài liệu JSON vs trường đc tạo ra
+// (trong trường hợp tên 2 thằng không giống nhau. Nếu ko dùng thg này kết quả của trường này sẽ ra null)
+//  (to specify the exact key of the JSON document)
+    @JsonProperty("type")
+    private String typess;
+//    @JsonProperty("value")
+    private Value vasdasdaluesssssssssssssss;
+
 }
