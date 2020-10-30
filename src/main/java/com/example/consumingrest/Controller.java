3ef3e48884bbbc08228e6a82144bada220939f59
nmson <nmson@ecpay.vn>
2020-10-30 08:20:53 +0700

    display receiver restdata

--- src/main/java/com/example/consumingrest/Controller.java
+++ src/main/java/com/example/consumingrest/Controller.java
@@ -1,5 +1,6 @@
 package com.example.consumingrest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,12 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 
 public class Controller {
+    @Autowired
+    private RestTemplate restTemplate;
     @GetMapping("/shit")
     @ResponseBody
     public String shit(){
-        System.out.println("shit");
-        RestTemplate restTemplate =new RestTemplate();
+//        Lấy ra dữ liệu từ API , gắn nó vào Object Quote
         Quote quote =restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random",Quote.class);
         return quote.toString();
     }
